3f7212cf4ae5b70a32218e404aa3c92233a4f349
Enrique Fernandez Perez <awsmr1605@gmail.com>
2021-05-06 09:57:19 +0200

    v8

--- src/main/java/com/example/appBack/Student/repositorio/StudentRepositoryImpl.java
+++ src/main/java/com/example/appBack/Student/repositorio/StudentRepositoryImpl.java
@@ -32,45 +32,52 @@ public class StudentRepositoryImpl
 
             String poner = "";//para recoger datos y limpieza visual de codigo
 
-            if (estudianteDto.getNombre().trim().length() != 0) {
+            if (estudianteDto.getNombre() != null  && estudianteDto.getNombre().trim().length() != 0) {
                 poner = estudianteDto.getNombre().trim();
                 predicates.add(cb.like(root.get("nombre"), "%" + poner + "%"));
             }
 
-           if (estudianteDto.getApellido().trim().length() != 0) {
+           if (estudianteDto.getApellido() != null  && estudianteDto.getApellido().trim().length() != 0)
+           {
                 poner = estudianteDto.getApellido().trim();
                 predicates.add(cb.equal(root.get("apellido"), "%" + poner + "%"));
             }
 
-            if (estudianteDto.getCorreo().trim().length() != 0) {
-                poner = estudianteDto.getCorreo().trim();
-                predicates.add(cb.like(root.get("correo"), "%" + poner + "%"));
-            }
+           if (estudianteDto.getCorreo() != null  && estudianteDto.getCorreo().trim().length() != 0)
+           {
+               poner = estudianteDto.getCorreo().trim();
+               predicates.add(cb.like(root.get("correo"), "%" + poner + "%"));
+           }
 
-            if (estudianteDto.getFecha_entrada() != null) {
-                Date fecha = estudianteDto.getFecha_entrada();
-                predicates.add(cb.equal(root.get("fecha_entrada"), fecha));
-            }
+           if (estudianteDto.getFecha_entrada() != null)
+           {
+               Date fecha = estudianteDto.getFecha_entrada();
+               predicates.add(cb.equal(root.get("fecha_entrada"), fecha));
+           }
 
-            if (estudianteDto.getCiudad().trim().length() != 0) {
-                poner = estudianteDto.getCiudad().trim();
-                predicates.add(cb.like(root.get("ciudad"), "%" + poner + "%"));
-            }
+           if (estudianteDto.getCiudad() != null  && estudianteDto.getCiudad().trim().length() != 0)
+           {
+               poner = estudianteDto.getCiudad().trim();
+               predicates.add(cb.like(root.get("ciudad"), "%" + poner + "%"));
+           }
 
-            if (estudianteDto.getHoras_semanales() != null) {
-                poner = estudianteDto.getHoras_semanales().toString().trim();
-                predicates.add(cb.equal(root.get("horas_semanales"), Integer.parseInt(poner)));
-            }
+           if (estudianteDto.getHoras_semanales() != null)
+           {
+               poner = estudianteDto.getHoras_semanales().toString().trim();
+               predicates.add(cb.equal(root.get("horas_semanales"), Integer.parseInt(poner)));
+           }
 
-            if (estudianteDto.getEspecialidad().trim().length() != 0) {
-                poner = "%" + estudianteDto.getEspecialidad().trim() + "%";
-                predicates.add(cb.like(root.get("especialidad"), poner));
-            }
+           if (estudianteDto.getEspecialidad() != null && estudianteDto.getEspecialidad().trim().length() != 0)
+           {
+               poner = "%" + estudianteDto.getEspecialidad().trim() + "%";
+               predicates.add(cb.like(root.get("especialidad"), poner));
+           }
 
-            if (estudianteDto.getEstado().trim().length() != 0) {
-                poner = "%" + estudianteDto.getEstado().trim() + "%";
-                predicates.add(cb.like(root.get("especialidad"), poner));
-            }
+           if (estudianteDto.getEstado() != null && estudianteDto.getEstado().trim().length() != 0)
+           {
+               poner = "%" + estudianteDto.getEstado().trim() + "%";
+               predicates.add(cb.like(root.get("especialidad"), poner));
+           }
 
             query.select(root).where(predicates.toArray(new Predicate[predicates.size()]));
             List<Student> lista = new ArrayList<>();
