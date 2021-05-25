130b7c5c9b83dfd8562365068d2c4dc728a0b4d3
Enrique Fernandez Perez <awsmr1605@gmail.com>
2021-05-25 10:36:44 +0200

    v0

--- src/main/java/com/example/appBack/AppBackApplication.java
+++ src/main/java/com/example/appBack/AppBackApplication.java
@@ -1,10 +1,9 @@
 package com.example.appBack;
 
-
-import com.example.appBack.Student.Entity.branch;
-import com.example.appBack.Student.Entity.Student;
-import com.example.appBack.Student.Entity.branch;
-import com.example.appBack.Student.repositorio.StudentRepository;
+import com.example.appBack.Tablas.Student.domain.Student;
+import com.example.appBack.Tablas.Student.domain.StudentJpa;
+import com.example.appBack.Tablas.Student.infrastructure.repositorio.JPA.StudentRepository;
+import com.example.appBack.noDatabase.BranchEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,37 +17,27 @@ import java.util.Optional;
 @SpringBootApplication
 @EnableSwagger2
 @EnableJpaRepositories
-public class AppBackApplication implements CommandLineRunner
+public class AppBackApplication //implements CommandLineRunner
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(AppBackApplication.class, args);
 	}
 
-	@Autowired
+	/*@Autowired
 	StudentRepository studentRepository;
 
 	@Override
 	public void run(String... arg0) throws Exception
 	{
-		studentRepository.saveAndFlush(new Student("1","Carlos","Garcia","dam06.2020@gmail.com", new Date(2021,4,27), "Logroño", 40, "BackEnd",true, "","",new Date(2031,4,27), branch.FRONT));
-		studentRepository.saveAndFlush(new Student("2","Manuel","Perez","dam12.2020@gmail.com", new Date(2021,4,27), "Logroño", 40, "BackEnd",true, "","",new Date(2031,4,27),branch.BACK));
-		studentRepository.saveAndFlush(new Student("3","Marta","Fernandez","dam2.2020@gmail.com", new Date(2021,4,28), "Logroño", 40, "FrontEnd",true, "","",new Date(2031,4,27),branch.BACK));
-		studentRepository.saveAndFlush(new Student("4","Luis","Ruiz","dam8.2020@gmail.com", new Date(2021,4,29), "Logroño", 40, "BacktEnd",true, "","",new Date(2031,4,27),branch.FRONT));
+		studentRepository.saveAndFlush(new StudentJpa(new Student("1","Carlos","Garcia","dam06.2020@gmail.com", new Date(2021,4,27), "Logroño", 40, "BackEnd",true, "","",new Date(2031,4,27), BranchEnum.FRONT,"")));
+		studentRepository.saveAndFlush(new StudentJpa(new Student("2","Manuel","Perez","dam12.2020@gmail.com", new Date(2021,4,27), "Logroño", 40, "BackEnd",true, "","",new Date(2031,4,27),BranchEnum.BACK,"")));
+		studentRepository.saveAndFlush(new StudentJpa(new Student("3","Marta","Fernandez","dam2.2020@gmail.com", new Date(2021,4,28), "Logroño", 40, "FrontEnd",true, "","",new Date(2031,4,27),BranchEnum.BACK,"")));
+		studentRepository.saveAndFlush(new StudentJpa(new Student("4","Luis","Ruiz","dam8.2020@gmail.com", new Date(2021,4,29), "Logroño", 40, "BacktEnd",true, "","",new Date(2031,4,27),BranchEnum.FRONT,"")));
 
-		studentRepository.saveAndFlush(new Student("5","Elsa","Perez","dam7.2020@gmail.com", new Date(20201,5,1), "Logroño", 40, "Business Intelligence",true, "","",new Date(2031,4,27),branch.DEVOPS));
-		studentRepository.saveAndFlush(new Student("6","Mario","Lazaro","dam4.2020@gmail.com", new Date(2021,5,1), "Logroño", 40, "DevOps",false, "","",new Date(2031,4,27),branch.UNASIGNED));
-		studentRepository.saveAndFlush(new Student("7","Lucia","Navarro","daw11.2020@gmail.com", new Date(2021,5,1), "Logroño", 40, "Recursos Humanos",false, "","",new Date(2031,4,27),branch.FRONT));
-		studentRepository.saveAndFlush(new Student("8","Alejandro","Martinez","daw02.2020@gmail.com", new Date(2021,5,2), "Logroño", 40, "Sistemas",true, "","",new Date(2031,4,27),branch.DEVOPS));
-
-		/*studentRepository.saveAndFlush(new Student("1","Carlos","Garcia","dam06.2020@gmail.com", new Date(2021,4,27), "Logroño", 40, "BackEnd","Activo", "","",new Date(2031,4,27), branch.FRONT));
-		studentRepository.saveAndFlush(new Student("2","Manuel","Perez","dam12.2020@gmail.com", new Date(2021,4,27), "Logroño", 40, "BackEnd","Activo", "","",new Date(2031,4,27),branch.BACK));
-		studentRepository.saveAndFlush(new Student("3","Marta","Fernandez","dam2.2020@gmail.com", new Date(2021,4,28), "Logroño", 40, "FrontEnd","Activo", "","",new Date(2031,4,27),branch.BACK));
-		studentRepository.saveAndFlush(new Student("4","Luis","Ruiz","dam8.2020@gmail.com", new Date(2021,4,29), "Logroño", 40, "BacktEnd","Activo", "","",new Date(2031,4,27),branch.FRONT));
-
-		studentRepository.saveAndFlush(new Student("5","Elsa","Perez","dam7.2020@gmail.com", new Date(20201,5,1), "Logroño", 40, "Business Intelligence","Activo", "","",new Date(2031,4,27),branch.DEVOPS));
-		studentRepository.saveAndFlush(new Student("6","Mario","Lazaro","dam4.2020@gmail.com", new Date(2021,5,1), "Logroño", 40, "DevOps","Inactivo", "","",new Date(2031,4,27),branch.UNASIGNED));
-		studentRepository.saveAndFlush(new Student("7","Lucia","Navarro","daw11.2020@gmail.com", new Date(2021,5,1), "Logroño", 40, "Recursos Humanos","Inactivo", "","",new Date(2031,4,27),branch.FRONT));
-		studentRepository.saveAndFlush(new Student("8","Alejandro","Martinez","daw02.2020@gmail.com", new Date(2021,5,2), "Logroño", 40, "Sistemas","Activo", "","",new Date(2031,4,27),branch.DEVOPS));*/
-	}
+		studentRepository.saveAndFlush(new StudentJpa(new Student("5","Elsa","Perez","dam7.2020@gmail.com", new Date(20201,5,1), "Logroño", 40, "Business Intelligence",true, "","",new Date(2031,4,27),BranchEnum.DEVOPS,"")));
+		studentRepository.saveAndFlush(new StudentJpa(new Student("6","Mario","Lazaro","dam4.2020@gmail.com", new Date(2021,5,1), "Logroño", 40, "DevOps",false, "","",new Date(2031,4,27),BranchEnum.UNASIGNED,"")));
+		studentRepository.saveAndFlush(new StudentJpa(new Student("7","Lucia","Navarro","daw11.2020@gmail.com", new Date(2021,5,1), "Logroño", 40, "Recursos Humanos",false, "","",new Date(2031,4,27),BranchEnum.FRONT,"")));
+		studentRepository.saveAndFlush(new StudentJpa(new Student("8","Alejandro","Martinez","daw02.2020@gmail.com", new Date(2021,5,2), "Logroño", 40, "Sistemas",true, "","",new Date(2031,4,27),BranchEnum.DEVOPS,"")));
+	}*/
 }
--- src/main/java/com/example/appBack/Student/Entity/branch.java
+++
@@ -1,3 +0,0 @@
-package com.example.appBack.Student.Entity;
-
-public enum branch {FRONT, BACK, DEVOPS, UNASIGNED}
